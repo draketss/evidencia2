dd77cadc4ffe8af15025b7b0cd7b9569849aa8d8
draketss <110877231+draketss@users.noreply.github.com>
2024-05-20 22:32:50 -0600

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,24 @@
+﻿function registro() {
+    var ru = $("#txtRegistro").val();
+    var nombre = $("#txtNombre").val();
+    var apellido = $("#txtApellido").val();
+    var contra = $("#txtContra").val();
+
+    $.ajax({
+        url: "Registro.aspx",
+        data: { ru: ru, name: nombre, ape: apellido, pass: contra },
+        type: "post",
+        success: function (data) {
+            if (data.substring(0, 10) === "Registrado") {
+                alert("Registrado correctamente");
+                location.href = "Login.aspx";
+            }
+            else
+                alert("Error al registrar en registro");
+        },
+        error: function (data) {
+            alert("Error: " + data);
+        }
+    });
+}
+
---
+++ Usuario.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(RegistroLoginAjax.Usuario))]
+
+namespace RegistroLoginAjax
+{
+    public class Usuario
+    {
+        private string ru;
+        private string nombre;
+        private string apellido;
+        private string contra;
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string ru, string nombre, string apellido, string contra)
+        {
+            this.ru = ru;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.contra = contra;
+        }
+
+        public string RU { get => ru; set => ru = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Contra { get => contra; set => contra = value; }
+    }
+}
+
+       
